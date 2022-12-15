476ec7ececbe070c809835333c6e984efa11edc0
Katrina Shyne Gurrea <katrinashynegurrea@gmail.com>
2022-12-15 16:48:00 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,13 +1,43 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+    public LeapYearGUI(){
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());
+                boolean leap;
+
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        leap = year % 400 == 0;
+                    } else
+                        leap = true;
+                } else
+                    leap = false;
+
+                if (leap)
+                    JOptionPane.showMessageDialog(panel1, "Leap year");
+                else
+                    JOptionPane.showMessageDialog(panel1, "Not a leap year");
+            }
+        });
+    }
+
     public static void main(String[] args) {
         LeapYearGUI leapYear = new LeapYearGUI();
+        leapYear.setContentPane(leapYear.panel1);
+        leapYear.setTitle("Leap Year Checker");
+        leapYear.setSize(450,300);
+        leapYear.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        leapYear.setVisible(true);
     }
 }
