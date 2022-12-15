78142e8610588c86aa0d714c32841d9cb201eeeb
Katrina Shyne Gurrea <katrinashynegurrea@gmail.com>
2022-12-15 17:46:26 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -10,11 +12,47 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                double num1 = Double.parseDouble(tfNumber1.getText());
+                double num2 = Double.parseDouble(tfNumber2.getText());
+                double result = 0;
+
+                switch (cbOperations.getSelectedIndex()) {
+                    case 0:
+                        result = num1 + num2;
+                        break;
+                    case 1:
+                        result = num1 - num2;
+                        break;
+                    case 2:
+                        result = num1 * num2;
+                        break;
+                    case 3:
+                        result = num1 / num2;
+                        break;
+                    }
+
+                if(String.valueOf(result).endsWith("0")){
+                    int noDec = (int) result;
+                    lblResult.setText(String.valueOf(noDec));
+                }
+                else{
+                    lblResult.setText(String.valueOf(result));
+                }
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI calc = new SimpleCalcGUI();
         calc.setContentPane(calc.panel1);
         calc.setTitle("Simple Calculator");
         calc.setSize(550, 250);
+        calc.lblResult.setEditable(false);
         calc.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         calc.setVisible(true);
     }
